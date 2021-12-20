cd393244080d0ff4199808d3bfd217afc22a8fb1
Carlos Freitas <carlos@carlos.freitas.(none)>
2021-12-20 13:13:40 -0300

    feat(hooks): added useForm hook.

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,36 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 
 const LoginForm = () => {
 
-  const [userName, setUserName] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({userName, password}),
-    }).then((response) => {
-      console.log(userName, password);
-      console.log(response);
-      return response.json();
-    }).then((json) => {
-      console.log(json);
-    })
+
+    if(username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      }).then((response) => {
+        return response.json();
+      }).then((json) => {
+        console.log(json);
+      });
+    }
+
   }
 
   return (
     <section>
       <h1> Login </h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuario" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
 
         <Button> Entrar </Button>
       </form>
