9bfb39be4470bb26940aab122e6470354bb76d60
Carlos Freitas <carlos@carlos.freitas.(none)>
2021-12-21 13:08:06 -0300

    feat(hooks): created usefetch hook.

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -4,6 +4,8 @@ import Input from '../Forms/Input';
 import useForm from '../../Hooks/useForm';
 import { USER_POST } from '../../api';
 import { UserContext } from '../../UserContext';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 
 
 const LoginCreate = () => {
@@ -13,6 +15,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const {userLogin} = React.useContext(UserContext);
+  const {loading, error, request} = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -21,9 +24,8 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value
     });
-    const response = await fetch(url, options);
+    const {response} = await request(url, options);
     if(response.ok) userLogin(username.value, password.value);
-    console.log(response);
   }
 
 
@@ -34,7 +36,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+
+        {loading ? <Button disabled>Cadastrando...</Button> : 
+        <Button>Cadastrar</Button>        
+        }
+
+        <Error error={error} />
       </form>
     </section>
   )
