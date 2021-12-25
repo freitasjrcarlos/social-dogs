7dbdb9616f6e00c81c7672d060e8e9bfd759ebc4
Carlos Freitas <hello@freitas.space>
2021-12-25 07:56:24 -0300

    feat(user): added user componentes style.

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -5,15 +5,29 @@ import {ReactComponent as MinhasFotos} from '../../Assets/feed.svg';
 import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg';
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg';
 import {ReactComponent as Sair} from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
+  const [mobile, setMobile] = React.useState(null);
   const {userLogout} = React.useContext(UserContext);
   return (
-    <nav>
-      <NavLink to="/conta"> <MinhasFotos />Minhas fotos </NavLink>
-      <NavLink to="/conta/estatisticas"> <Estatisticas /> Estatísticas </NavLink>
-      <NavLink to="/conta/postar"> <AdicionarFoto />Adicionar Foto </NavLink>
-      <button onClick={userLogout}> <Sair /> Sair </button>
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end> 
+        <MinhasFotos />
+        {mobile && 'Minhas fotos'}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        {mobile && 'Estatísticas'}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        {mobile && 'Adicionar Foto'} 
+      </NavLink>
+      <button onClick={userLogout}>
+        <Sair /> 
+        {mobile && 'Sair'} 
+      </button>
     </nav>
   )
 }
